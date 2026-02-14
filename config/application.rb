31d8feff7db42d673f3283870d23bdfc25580f3d
Juliana Ifionu <julieifionu@gmail.com>
2026-02-14 18:45:31 +0000

    Initialize a Rails API only app

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SecuredCreditCardApi
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+    config.autoload_lib(ignore: %w(assets tasks))
+    config.api_only = true
+  end
+end
